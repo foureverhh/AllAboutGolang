1bfeac7172987029c63854a6cfb63f0f34fd6326
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-08 11:18:50 +0200

    Tested callback

---
+++ goLearning/ConcurrentProgrammingWithGo/ConcurrentPatterns/CallbacksDemo/demo.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	po := new (PurchaseOrder)
+	po.Value = 42.27
+
+	ch := make(chan *PurchaseOrder)
+	go SavePO(po,ch)
+	newPo := <- ch
+	fmt.Printf("PO: %v",newPo)
+}
+
+//PurchaseOrder struct
+type PurchaseOrder struct{
+	Number int
+	Value float64
+}
+//SavePO to save PurchaseOrder
+func SavePO(po *PurchaseOrder, callback chan *PurchaseOrder)  {
+	po.Number = 1234
+	callback <- po
+}
